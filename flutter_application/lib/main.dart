ce674a06d6dacbe70bf42e307d03aacfbb9201ed
Magnus Axelsen <maxels19@student.aau.dk>
2022-03-17 14:18:50 +0100

    Removed unused stuff

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -5,7 +5,6 @@ import 'dart:ui';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:native_opencv/native_opencv.dart';
 import 'package:flutter_application/camera.dart';
 import 'package:flutter_application/cameraPage/cameraPage.dart';
 import 'package:flutter_application/homeScreen/homeScreen.dart';
@@ -13,7 +12,6 @@ import 'package:flutter_application/homeScreen/homeScreen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  NativeOpenCv nativeOpenCv = NativeOpenCv();
   
   // Obtain a list of the available cameras on the device.
   final cameras = await availableCameras();
