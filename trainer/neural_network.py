562bc6b6705138d4b1535e44b784bee3910b8818
sandborg <laurits1994@gmail.com>
2022-04-07 09:53:18 +0200

    [udpate] now matches the layers sued on dev branch

--- trainer/neural_network.py
+++ trainer/neural_network.py
@@ -8,26 +8,29 @@ from helper_functions.loss_function import mse, mse_prime
 from sklearn.model_selection import train_test_split
 
 dataset = pd.read_json("preprocess/labels.json")
-x = dataset.iloc[:, 6:].values
-y = dataset.iloc[:, 2:6].values
+x = dataset.iloc[:, 3:].values
+y = dataset.iloc[:, 2:3].values
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
-x_train = x_train.reshape(x_train.shape[0], 1, 1*5)
+x_train = x_train.reshape(x_train.shape[0], 1, 1*8)
 x_train = x_train.astype('float32')
 
-x_test = x_test.reshape(x_test.shape[0], 1, 1*5)
+x_test = x_test.reshape(x_test.shape[0], 1, 1*8)
 x_test = x_test.astype('float32')
 
+
 net = Network()
-net.add(FCLayer(5, 8))
+net.add(FCLayer(8, 5))
 net.add(Activation_Layer(tanh, tanh_prime))
-net.add(FCLayer(8, 1))
+net.add(FCLayer(5, 3))
+net.add(Activation_Layer(tanh, tanh_prime))
+net.add(FCLayer(3, 1))
 net.add(Activation_Layer(tanh, tanh_prime))
 
 
 net.use(mse, mse_prime)
-net.fit(x_train, y_train, epochs=5000, learning_rate=0.1)
+net.fit(x_train, y_train, epochs=1000, learning_rate=0.1)
 
 out = net.predict(x_test)
 
